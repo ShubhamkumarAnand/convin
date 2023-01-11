db47aeb3d2f6d508b89344b7527111666fdc5a9d
imskanand <the.anand3099@gmail.com>
2023-01-11 21:21:10 +0530

    Accessed the card component using the axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import './App.css'
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState([])
+  const fetchData = () => {
+    return axios.get("http://localhost:3000/cards")
+      .then((response) => setCards(response.data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  console.log(cards);
   return (
-    <div className="App">
-      Hello Covin
+    <div className='App'>
+      <ul>
+        { cards.map(card =>
+          <li key={ card.id }>
+            {card.id} <br />
+            { card.name } <br />
+            {card.link}
+          </li>
+        ) }
+      </ul>
     </div>
   )
 }
